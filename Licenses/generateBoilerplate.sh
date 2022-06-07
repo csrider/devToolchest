4c93597f43b794b0839e52dfd5ad547ccaced5aa
Chris Rider <csrider@gmail.com>
2022-06-07 18:01:38 -0400

    fixed cosmetic bug with license gen script

--- Licenses/generateBoilerplate.sh
+++ Licenses/generateBoilerplate.sh
@@ -21,9 +21,8 @@
 ##
 ##  Copyright 2022 Chris Rider (csrider@gmail.com)
 ##
-##  This Source Code Form is subject to the terms of the Mozilla Public
-##  License, v. 2.0. If a copy of the MPL was not distributed with this file,
-##  you can obtain one at http://mozilla.org/MPL/2.0/.
+##  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not 
+##  distributed with this file, you can obtain one at http://mozilla.org/MPL/2.0/.
 ##
 ########################################################################################################################
 
